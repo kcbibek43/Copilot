2dee44be824a1cc60bc3b50891560bb702d926ef
kcbibek43 <101321124+kcbibek43@users.noreply.github.com>
2024-01-25 03:54:50 -0800

    Add files via upload

---
+++ CopilotProject/CopilotProject/Controller/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Models;
+using YourNamespace.Services;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UsersService _usersService;
+
+        public AuthController(UsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(User user)
+        {
+            var existingUser = _usersService.GetByEmail(user.Email);
+            if (existingUser != null)
+            {
+                return BadRequest("User already exists with this email.");
+            }
+
+            user = _usersService.Create(user);
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserLogin userLogin)
+        {
+            var user = _usersService.GetByEmailAndPassword(userLogin.Email, userLogin.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            
+            return user;
+        }
+    }
+}
---
+++ CopilotProject/CopilotProject/Services/MessageServices.cs
@@ -0,0 +1,40 @@
+using MongoDB.Driver;
+using YourNamespace.Model;
+using YourNamespace.Models;
+
+namespace YourNamespace.Services {
+    public class MessagesService
+    {
+    private readonly IMongoCollection<Messages> _messages;
+
+    public MessagesService(MongoDBSettings settings)
+    {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _messages = database.GetCollection<Messages>("Messages");
+    }
+
+    public List<Messages> Get() =>
+        _messages.Find(messages => true).ToList();
+
+    public Messages Get(string id) =>
+        _messages.Find<Messages>(messages => messages.Id == id).FirstOrDefault();
+
+    public Messages Create(Messages messages)
+    {
+        _messages.InsertOne(messages);
+        return messages;
+    }
+
+    public void Update(string id, Messages messagesIn) =>
+        _messages.ReplaceOne(messages => messages.Id == id, messagesIn);
+
+    public void Remove(Messages messagesIn) =>
+        _messages.DeleteOne(messages => messages.Id == messagesIn.Id);
+
+    public void Remove(string id) => 
+        _messages.DeleteOne(messages => messages.Id == id);
+    }
+
+}
